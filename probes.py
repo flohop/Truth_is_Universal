1f6ab7583baf79567a71db839aec2bf6f5ba3d88
flohop <hoppe.florian02@gmail.com>
2025-10-09 17:37:59 +0100

    next try

--- probes.py
+++ probes.py
@@ -1,37 +1,97 @@
 from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler, PolynomialFeatures
+from sklearn.linear_model import LogisticRegression
+
 import torch
 import torch as t
 import numpy as np
-from sklearn.linear_model import LogisticRegression
-from sklearn.preprocessing import StandardScaler, PolynomialFeatures
-from sympy.physics.units import degree
 
 from utils import dataset_sizes, collect_training_data
-
 import ray
 from ray import tune
-from ray.tune.tune import BasicVariantGenerator
 
+# -----------------------------------------------------------
+# Feature Interaction Helper
+# -----------------------------------------------------------
+def make_interaction_features(acts_2d, interaction_names):
+    """Generate interaction features dynamically from names."""
+    feature_dict = {
+        'proj_t_g': acts_2d[:, 0:1],
+        'proj_t_p': acts_2d[:, 1:2],
+        'proj_p':   acts_2d[:, 2:3],
+        'inter1':   acts_2d[:, 3:4]
+    }
 
+    features = []
+    for name in interaction_names:
+        if '+' in name:
+            a, b = name.split('+')
+            features.append(feature_dict[a] + feature_dict[b])
+        elif '-' in name:
+            a, b = name.split('-')
+            features.append(feature_dict[a] - feature_dict[b])
+        elif '*' in name:
+            a, b = name.split('*')
+            features.append(feature_dict[a] * feature_dict[b])
+        else:
+            features.append(feature_dict[name])
+    return np.concatenate(features, axis=1)
+
+# -----------------------------------------------------------
+# TTPDTest Probe
+# -----------------------------------------------------------
+class TTPDTest:
+    def __init__(self):
+        self.t_g = None
+        self.t_p = None
+        self.polarity_direc = None
+        self.LR = None
+
+    def _project_acts(self, acts):
+        proj_t_g = (acts.numpy() @ self.t_g.numpy())[:, None]
+        proj_p = acts.numpy() @ self.polarity_direc.T
+        proj_t_p = (acts.numpy() @ self.t_p.numpy())[:, None]
+        inter1 = proj_t_p * proj_p
+        return np.concatenate([proj_t_g, proj_t_p, proj_p, inter1], axis=1)
+
+# -----------------------------------------------------------
+# Direction Learners
+# -----------------------------------------------------------
+def learn_truth_directions(acts_centered, labels, polarities):
+    all_polarities_zero = t.allclose(polarities, t.tensor([0.0]), atol=1e-8)
+    labels_copy = t.where(labels == 0, t.tensor(-1.0), labels)
+    X = labels_copy.reshape(-1,1) if all_polarities_zero else t.column_stack([labels_copy, labels_copy*polarities])
+    solution = t.linalg.inv(X.T @ X) @ X.T @ acts_centered
+    if all_polarities_zero:
+        return solution.flatten(), None
+    else:
+        return solution[0,:], solution[1,:]
+
+def learn_polarity_direction(acts, polarities):
+    polarities_copy = polarities.clone()
+    polarities_copy[polarities_copy == -1] = 0
+    LR = LogisticRegression(penalty='l2', C=0.1, fit_intercept=True, solver='lbfgs', max_iter=2000)
+    LR.fit(acts.numpy(), polarities_copy.numpy())
+    return LR.coef_
+
+# -----------------------------------------------------------
+# Training function for Ray Tune
+# -----------------------------------------------------------
 def train_ttpd_test(config, acts_centered_train, acts_train, labels_train, polarities_train,
                     acts_centered_val, acts_val, labels_val, polarities_val):
     probe = TTPDTest()
-
-    # Learn truth directions on training set
     probe.t_g, probe.t_p = learn_truth_directions(acts_centered_train, labels_train, polarities_train)
-    probe.t_g = probe.t_g.numpy()
-    probe.t_p = probe.t_p.numpy()
-
-    # Learn polarity direction on training set
     probe.polarity_direc = learn_polarity_direction(acts_train, polarities_train)
 
-    # Project activations for training
+    # Project and compute interaction features
     acts_2d_train = probe._project_acts(acts_train)
     acts_2d_val = probe._project_acts(acts_val)
 
-    # Build LR pipeline from config
+    X_train = make_interaction_features(acts_2d_train, config['interaction_features'])
+    X_val   = make_interaction_features(acts_2d_val, config['interaction_features'])
+
+    # Build pipeline
     steps = []
     if config['use_scaler']:
         steps.append(('scaler', StandardScaler()))
@@ -43,330 +103,65 @@ def train_ttpd_test(config, acts_centered_train, acts_train, labels_train, polar
         solver=config['solver'],
         max_iter=2000
     )))
-
     probe.LR = Pipeline(steps)
-    probe.LR.fit(acts_2d_train, labels_train.numpy())
 
-    # Evaluate on validation set
-    preds_val = probe.LR.predict(acts_2d_val)
+    # Fit and evaluate
+    probe.LR.fit(X_train, labels_train.numpy())
+    preds_val = probe.LR.predict(X_val)
     acc_val = (preds_val == labels_val.numpy()).mean()
 
     tune.report({"accuracy": acc_val})
 
-
-def learn_truth_directions(acts_centered, labels, polarities):
-    # Check if all polarities are zero (handling both int and float) -> if yes learn only t_g
-    all_polarities_zero = t.allclose(polarities, t.tensor([0.0]), atol=1e-8)
-    # Make the sure the labels only have the values -1.0 and 1.0, replace 0 with -1
-    labels_copy = labels.clone()
-    labels_copy = t.where(labels_copy == 0.0, t.tensor(-1.0), labels_copy)
-    
-    if all_polarities_zero:
-        X = labels_copy.reshape(-1, 1)
-    else:
-        X = t.column_stack([labels_copy, labels_copy * polarities])
-
-    # Compute the analytical OLS solution
-    # find the linear coefficients that best explain activations in terms of truth and polarity
-    solution = t.linalg.inv(X.T @ X) @ X.T @ acts_centered
-
-    # Extract t_g and t_p
-    if all_polarities_zero:
-        t_g = solution.flatten()
-        t_p = None
-    else:
-        t_g = solution[0, :]
-        t_p = solution[1, :]
-
-    # weights that can be applied to the activation vector
-    return t_g, t_p
-
-
-def polarity_direction_lr(acts, polarities):
-    polarities_copy = polarities.clone()
-    polarities_copy[polarities_copy == -1.0] = 0.0
-    LR_polarity = LogisticRegression(penalty='l2', C=0.1, fit_intercept=True, solver='lbfgs', max_iter=2000)
-    #LR_polarity = LogisticRegression(penalty=None, fit_intercept=True)
-    LR_polarity.fit(acts.numpy(), polarities_copy.numpy())
-    return LR_polarity
-
-def learn_polarity_direction(acts, polarities):
-    polarity_direc = polarity_direction_lr(acts, polarities).coef_
-    return polarity_direc
-
-
-def measure_polarity_direction_lr(train_acts, train_polarities, valid_acts, valid_polarities):
-    LR = polarity_direction_lr(train_acts, train_polarities)
-
-    pred = LR.predict(valid_acts)
-    num_diff = np.sum(~np.isclose(pred, valid_polarities))
-    return 1 -  num_diff / len(valid_acts)
-
-
-class TTPDTest:
-    def __init__(self):
-        self.t_g = None
-        self.t_p = None
-        self.polarity_direc = None
-        self.LR = None
-
-    @staticmethod
-    def from_data(acts_centered, acts, labels, polarities):
-        probe = TTPDTest()
-
-        # Learn truth directions
-        probe.t_g, probe.t_p = learn_truth_directions(acts_centered, labels, polarities)
-        probe.t_g = probe.t_g.numpy()
-        probe.t_p = probe.t_p.numpy()
-
-        # Learn polarity direction
-        probe.polarity_direc = learn_polarity_direction(acts, polarities)
-
-        # Project activations
-        acts_2d = probe._project_acts(acts)
-
-        # Full pipeline with scaling + polynomial features
-        probe.LR = Pipeline([
-            ('scaler', StandardScaler()),
-            ('poly', PolynomialFeatures(degree=1, include_bias=True)),
-            ('lr', LogisticRegression(C=0.1959, penalty='l2', fit_intercept=True, solver="saga", max_iter=2000))
-        ])
-
-        # Fit on all 4 features
-        probe.LR.fit(acts_2d, labels.numpy())
-        return probe
-
-    def pred(self, acts):
-        acts_2d = self._project_acts(acts)
-        return t.tensor(self.LR.predict(acts_2d))
-
-    def _project_acts(self, acts):
-        proj_t_g = (acts.numpy() @ self.t_g)[:, None]  # general truth
-        proj_p = (acts.numpy() @ self.polarity_direc.T)  # polarity
-        proj_t_p = (acts.numpy() @ self.t_p)[:, None] * proj_p  # interaction
-        # inter1 = proj_t_g + proj_t_p  # simple additive interaction
-        acts_2d = np.concatenate((proj_t_g, proj_t_p, proj_p), axis=1)
-        return acts_2d
-
-
-
-class TTPD():
-    # Force LR to only use truth and polarity dimensions
-    def __init__(self):
-        self.t_g = None
-        self.polarity_direc = None
-        self.LR = None
-
-    @staticmethod
-    def from_data(acts_centered, acts, labels, polarities):
-        probe = TTPD()
-        # Learn direction for truth
-        probe.t_g, _ = learn_truth_directions(acts_centered, labels, polarities)
-        probe.t_g = probe.t_g.numpy()
-
-        # Learn direction for polarity
-        probe.polarity_direc = learn_polarity_direction(acts, polarities)
-
-        # Project all dimensions onto the 2D truth/polarity space
-        acts_2d = probe._project_acts(acts)
-
-        # Fit logistic regression
-        probe.LR = LogisticRegression(penalty=None, fit_intercept=True)
-        probe.LR.fit(acts_2d, labels.numpy())
-        return probe
-
-    def pred(self, acts):
-        acts_2d = self._project_acts(acts)
-        return t.tensor(self.LR.predict(acts_2d))
-
-    def _project_acts(self, acts):
-        proj_t_g = acts.numpy() @ self.t_g
-        proj_p = acts.numpy() @ self.polarity_direc.T
-        acts_2d = np.concatenate((proj_t_g[:, None], proj_p), axis=1)
-        return acts_2d
-
-def ccs_loss(probe, acts, neg_acts):
-    p_pos = probe(acts)
-    p_neg = probe(neg_acts)
-    consistency_losses = (p_pos - (1 - p_neg)) ** 2
-    confidence_losses = t.min(t.stack((p_pos, p_neg), dim=-1), dim=-1).values ** 2
-    return t.mean(consistency_losses + confidence_losses)
-
-
-class CCSProbe(t.nn.Module):
-    def __init__(self, d_in):
-        super().__init__()
-        self.net = t.nn.Sequential(
-            t.nn.Linear(d_in, 1, bias=True),
-            t.nn.Sigmoid()
-        )
-    
-    def forward(self, x, iid=None):
-        return self.net(x).squeeze(-1)
-    
-    def pred(self, acts, iid=None):
-        return self(acts).round()
-    
-    def from_data(acts, neg_acts, labels=None, lr=0.001, weight_decay=0.1, epochs=1000, device='cpu'):
-        acts, neg_acts = acts.to(device), neg_acts.to(device)
-        probe = CCSProbe(acts.shape[-1]).to(device)
-        
-        opt = t.optim.AdamW(probe.parameters(), lr=lr, weight_decay=weight_decay)
-        for _ in range(epochs):
-            opt.zero_grad()
-            loss = ccs_loss(probe, acts, neg_acts)
-            loss.backward()
-            opt.step()
-
-        if labels is not None: # flip direction if needed
-            labels = labels.to(device)
-            acc = (probe.pred(acts) == labels).float().mean()
-            if acc < 0.5:
-                probe.net[0].weight.data *= -1
-        
-        return probe
-
-    @property
-    def direction(self):
-        return self.net[0].weight.data[0]
-    
-    @property
-    def bias(self):
-        return self.net[0].bias.data[0]
-    
-
-class LRProbe():
-    def __init__(self):
-        self.LR = None
-
-    def from_data(acts, labels):
-        probe = LRProbe()
-        probe.LR = LogisticRegression(penalty=None, fit_intercept=True)
-        probe.LR.fit(acts.numpy(), labels.numpy())
-        return probe
-
-    def pred(self, acts):
-        return t.tensor(self.LR.predict(acts))
-    
-
-class MMProbe(t.nn.Module):
-    def __init__(self, direction, LR):
-        super().__init__()
-        self.direction = direction
-        self.LR = LR
-
-    def forward(self, acts):
-        proj = acts @ self.direction
-        return t.tensor(self.LR.predict(proj[:, None]))
-
-    def pred(self, x):
-        return self(x).round()
-
-    def from_data(acts, labels, device='cpu'):
-        acts, labels
-        pos_acts, neg_acts = acts[labels==1], acts[labels==0]
-        pos_mean, neg_mean = pos_acts.mean(0), neg_acts.mean(0)
-        direction = pos_mean - neg_mean
-        # project activations onto direction
-        proj = acts @ direction
-        # fit bias
-        LR = LogisticRegression(penalty=None, fit_intercept=True)
-        LR.fit(proj[:, None], labels)
-        
-        probe = MMProbe(direction, LR).to(device)
-
-        return probe
-
-# (title, object)
-TTPD_TYPES = [
-        # ("TTPD", TTPD),
-        ("TTPDAffirm", TTPDTest)
-            ]
-
-# To speed up testing, for full report use all probes
-# ALL_PROBES = TTPD_TYPES + [("LRProbe", LRProbe), ("CCSProbe", CCSProbe), ("MMProbe", MMProbe)]
-ALL_PROBES = TTPD_TYPES
-
-
+# -----------------------------------------------------------
+# Main execution
+# -----------------------------------------------------------
 if __name__ == '__main__':
-    model_family = 'Llama3'  # options are 'Llama3', 'Llama2', 'Gemma', 'Gemma2' or 'Mistral'
+    model_family = 'Llama3'
     model_size = '8B'
-    model_type = 'chat'  # options are 'chat' or 'base'
-    layer = 12  # layer from which to extract activations
+    model_type = 'chat'
+    layer = 12
+    device = 'mps' if torch.has_mps else 'cpu'
 
-    device = 'mps' if torch.mps.is_available() else 'cpu'  # gpu speeds up CCS training a fair bit but is not required
-
+    train_sets = ["cities", "neg_cities", "sp_en_trans", "neg_sp_en_trans", "inventors", "neg_inventors",
+                  "animal_class", "neg_animal_class", "element_symb", "neg_element_symb", "facts", "neg_facts"]
     val_sets = ["cities_conj", "cities_disj", "sp_en_trans_conj", "sp_en_trans_disj",
                 "inventors_conj", "inventors_disj", "animal_class_conj", "animal_class_disj",
                 "element_symb_conj", "element_symb_disj", "facts_conj", "facts_disj",
                 "common_claim_true_false", "counterfact_true_false"]
 
-
+    train_set_sizes = dataset_sizes(train_sets)
     val_set_sizes = dataset_sizes(val_sets)
 
-    # define datasets used for training
-    train_sets = ["cities", "neg_cities", "sp_en_trans", "neg_sp_en_trans", "inventors", "neg_inventors",
-                  "animal_class",
-                  "neg_animal_class", "element_symb", "neg_element_symb", "facts", "neg_facts"]
-    train_set_sizes = dataset_sizes(train_sets)
-
-
-
-    # load training data
-    acts_centered_train, acts_train, labels_train, polarities_train = collect_training_data(train_sets, train_set_sizes,
-                                                                                            model_family, model_size,
-                                                                                            model_type, layer)
-
-    acts_centered_val, acts_val, labels_val, polarities_val = collect_training_data(val_sets, val_set_sizes,
-                                                                                    model_family, model_size,
-                                                                                    model_type, layer)
+    acts_centered_train, acts_train, labels_train, polarities_train = collect_training_data(
+        train_sets, train_set_sizes, model_family, model_size, model_type, layer
+    )
+    acts_centered_val, acts_val, labels_val, polarities_val = collect_training_data(
+        val_sets, val_set_sizes, model_family, model_size, model_type, layer
+    )
 
     ray.init(ignore_reinit_error=True)
 
-    # Hyperparameter search space
     search_space = {
-        # Logistic Regression hyperparameters
         'C': tune.loguniform(1e-3, 10),
         'fit_intercept': tune.choice([True, False]),
         'solver': tune.choice(['lbfgs', 'saga', 'liblinear']),
-
-        # Polynomial features
-        'degree': tune.choice([1, 2, 3]),
+        'degree': tune.choice([1,2,3]),
         'include_bias': tune.choice([True, False]),
-
-        # Scaling options
         'use_scaler': tune.choice([True, False]),
-
-        # Feature / interaction combinations
         'interaction_features': tune.choice([
-            # main effects only
             ['proj_t_g', 'proj_t_p'],
             ['proj_t_g', 'proj_t_p', 'proj_p'],
             ['proj_t_g', 'proj_t_p', 'inter1'],
-
-            # simple pairwise interactions
             ['proj_t_g', 'proj_t_p', 'proj_t_g+proj_t_p'],
             ['proj_t_g', 'proj_t_p', 'proj_t_g-proj_t_p'],
             ['proj_t_g', 'proj_t_p', 'proj_t_g*proj_t_p'],
-
-            # interactions with polarity
             ['proj_t_g', 'proj_t_p', 'proj_p', 'proj_t_g*proj_p'],
             ['proj_t_g', 'proj_t_p', 'proj_p', 'proj_t_p*proj_p'],
             ['proj_t_g', 'proj_t_p', 'proj_p', 'proj_t_g*proj_t_p*proj_p'],
-
-            # include additive interaction term from TTPDTest
             ['proj_t_g', 'proj_t_p', 'proj_p', 'inter1'],
-            ['proj_t_g', 'proj_t_p', 'proj_p', 'inter1', 'proj_t_g*proj_p'],
-            ['proj_t_g', 'proj_t_p', 'proj_p', 'inter1', 'proj_t_p*proj_p'],
-            ['proj_t_g', 'proj_t_p', 'proj_p', 'inter1', 'proj_t_g*proj_t_p*proj_p'],
-
-            # higher-order combos
-            ['proj_t_g', 'proj_t_p', 'proj_p', 'inter1', 'proj_t_g*proj_p', 'proj_t_g*proj_t_p'],
-            ['proj_t_g', 'proj_t_p', 'proj_p', 'inter1', 'proj_t_g+proj_t_p', 'proj_t_p*proj_p']
         ])
     }
 
-    # Run Ray Tune
     analysis = tune.run(
         tune.with_parameters(
             train_ttpd_test,
@@ -386,4 +181,4 @@ if __name__ == '__main__':
         config=search_space
     )
 
-    print("Best config found: ", analysis.best_config)
+    print("Best config found:", analysis.best_config)
